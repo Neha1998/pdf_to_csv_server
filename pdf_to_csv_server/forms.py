d7a67dd5b092738db1d635b4b10b6ff19387c753
Neha <neha.singla@delhivery.com>
2020-04-27 02:10:18 +0530

    Forms validations

--- pdf_to_csv_server/forms.py
+++ pdf_to_csv_server/forms.py
@@ -1,8 +1,40 @@
 from django import forms
 
-#from .models import Book
+UPLOAD_EXTENSIONS = ["pdf"]
 
 
-class PDFForm(forms.ModelForm):
-    class Meta:
-        fields = ('variable', 'year', 'pdf')
+class PdfForm(forms.Form):
+
+    year = forms.IntegerField(label="Enter Year",
+                                  required=True)
+
+    variable  = year = forms.IntegerField(label="Enter Year",
+                                  required=True)
+
+    document = forms.FileField(label="Balance Sseet",
+                                  required=True)
+
+    def clean_document(self):
+        """
+        Enforce file type
+        :return:
+        """
+        curr_file = self.cleaned_data['document']
+        if not enforce_file_extension(curr_file.name, UPLOAD_EXTENSIONS):
+            raise forms.ValidationError('Invalid file format. '
+                                        'Can only upload file types {}'.\
+                                format(' '.join(UPLOAD_EXTENSIONS)))
+
+    
+def enforce_file_extension(file_name, allowed_file_types):
+    if not file_name:
+        return False
+
+    file_name = file_name.split('.')
+
+    extension = file_name[-1]
+
+    if extension in allowed_file_types:
+        return True
+
+    return False
--- pdf_to_csv_server/views.py
+++ pdf_to_csv_server/views.py
@@ -4,7 +4,7 @@ from django.core.files.storage import FileSystemStorage
 from django.urls import reverse_lazy
 from .settings import MEDIA_URL,  BASE_DIR
 from .utils import pdf_to_csv
-#from .forms import BookForm
+from .forms import PdfForm
 #from .models import Book
 
 
@@ -12,25 +12,39 @@ from .utils import pdf_to_csv
 #     template_name = 'home.html'
 
 
+# def upload(request):
+#     context = {}
+#     if request.method == 'POST':
+#         uploaded_file = request.FILES['document']
+#         fs = FileSystemStorage()
+#         name = fs.save(uploaded_file.name, uploaded_file)
+        
+#         pdf_to_csv(BASE_DIR+MEDIA_URL+name)
+#         context['url'] = fs.url(name).replace(".pdf",".csv")
+#     return render(request, 'index.html', context)
+
+
+def calculate(request):
+    uploaded_file = request.FILES['document']
+    fs = FileSystemStorage()
+    name = fs.save(uploaded_file.name, uploaded_file)
+    
+    pdf_to_csv(BASE_DIR+MEDIA_URL+name)
+    context['url'] = fs.url(name).replace(".pdf",".csv")
+    return render(request, 'index.html', context)
+
 def upload(request):
     context = {}
     if request.method == 'POST':
-        uploaded_file = request.FILES['document']
+        form = PdfForm(request.POST, request.FILES)
+        if form.is_valid():
+            uploaded_file = request.FILES['document']
         fs = FileSystemStorage()
         name = fs.save(uploaded_file.name, uploaded_file)
         
         pdf_to_csv(BASE_DIR+MEDIA_URL+name)
         context['url'] = fs.url(name).replace(".pdf",".csv")
-    return render(request, 'index.html', context)
-
-# def upload_book(request):
-#     if request.method == 'POST':
-#         form = PdfForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('book_list')
-#     else:
-#         form = PdfForm()
-#     return render(request, 'upload_book.html', {
-#         'form': form
-#     })
+        return render(request, 'index.html', context)   
+    else:
+        form = PdfForm()
+        return render(request, 'index.html', {'form': form}, context)
